585f068a897b4606bee6bd64249fa3a3f96e0c6a
thejordanbautista <jblazo23@gmail.com>
2024-01-18 20:35:08 -0800

    connecting sql

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,12 +11,13 @@ const app = express();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'thejordanbautista', 
-  host: 'localhost',         
-  database: 'postgres', 
-  password: 'Pentagames00*', 
-  port: 5432,               
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
+
 
 app.use(cors());
 app.use(bodyParser.json());
